35d24a3dfea54e1065a80d2cc71b5f32055f7cc9
sedabull <sedabull@gmail.com>
2020-09-03 21:02:25 -0400

    added a few self-promotional links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import './App.css';
+
 import React from 'react';
 import Calculator from './components/calculator/Calculator';
 
 function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );//end return JSX
+    return (
+        <div className="App">
+            <h1>A fully functional calculator built with create-react-app!</h1>
+            <Calculator />
+            <p>
+                Created by: <a href="https://www.linkedin.com/in/seth-bullock-b893961ab/">Seth David Bullock</a>
+            </p>
+            <p>
+                Github repository: <a href="https://github.com/sedabull/calculator">https://github.com/sedabull/calculator</a>
+            </p>
+        </div>
+    );//end return JSX
 }//end App
 
 export default App;
